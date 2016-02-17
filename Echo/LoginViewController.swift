01fc165759e9846afc7b73b424624e17eba2ab1e
Ying Qi <yq25@duke.edu>
2016-02-17 09:52:58 -0500

    incorporated firebase

---
+++ Echo/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Echo
+//
+//  Created by Ying Qi on 2/17/16.
+//  Copyright © 2016 Duke University. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+    
+    }
+    
+    
+    
+    
+
+
+}
---
+++ Echo/Voice.swift
@@ -0,0 +1,17 @@
+//
+//  Voice.swift
+//  Echo
+//
+//  Created by Ying Qi on 2/17/16.
+//  Copyright © 2016 Duke University. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Voice {
+    
+    
+    
+    
+}
